02537b64c04cb0269cf38161c0bc757d4c7d3ba5
learner-syh <83892637+learner-syh@users.noreply.github.com>
2022-11-06 22:30:48 +0800

    Delete assign4 directory

--- assign4/demo/GUIConfig.h
+++
@@ -1,6 +0,0 @@
-
-MENU_OPTION("Mapping Earthquakes", makeEarthquakeGUI)
-MENU_OPTION("Child Mortality", makeChildMortalityGUI)
-MENU_OPTION("Women's 800m", makeWomens800mGUI)
-
-WINDOW_TITLE("Priority Queue Demos")
